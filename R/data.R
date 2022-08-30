e1670a43e3e3c778371c5b8ffd65140561e89b9f
Jim McLean <jim_mclean@optusnet.com.au>
2022-08-30 16:18:53 +1000

    Doco

--- R/data.R
+++ R/data.R
@@ -1,14 +1,15 @@
 #' Insulin data
 #'
+#' Results of experiments testing whether insulin reduces blood sugar.
+#'
 #' @format A data frame with 30 observations and 4 variables:
 #' \describe{
-#'   \item{sugar}{}
-#'   \item{time}{}
-#'   \item{id}{}
-#'   \item{experiment}{}
+#'   \item{sugar}{Measured blood sugar level}
+#'   \item{time}{Time of blood sugar measurements; \code{before} or \code{after} administering insulin}
+#'   \item{id}{Identifier of indivdual being measured (not in original data set)}
 #' }
 #'
-#' @source TODO
+#' @source TODO reference
 "insulin"
 
 #' Darwin data TODO
