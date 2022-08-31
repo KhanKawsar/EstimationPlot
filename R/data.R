cdd08010fb849efd06c1892e00dc3ac613f7599c
KhanKawsar <md-kawsar.khan@students.mq.edu.au>
2022-08-31 17:16:43 +1000

    Update data.R
    update insulin 2

--- R/data.R
+++ R/data.R
@@ -2,14 +2,16 @@
 #'
 #' Results of experiments testing whether insulin reduces blood sugar.
 #'
-#' @format A data frame with 30 observations and 4 variables:
+#' @format A data frame with 104 observations and 5 variables. Insulin was administered in rabbits and blood sugar was measured within three hours. Data collated from Table 1 and Table 2 of Banting et al., (1922)
 #' \describe{
 #'   \item{sugar}{Measured blood sugar level}
-#'   \item{time}{Time of blood sugar measurements; \code{before} or \code{after} administering insulin}
+#'   \item{treatment}{blood sugar measurements tretment group; \code{before} or \code{after} administering insulin}
 #'   \item{id}{Identifier of indivdual being measured (not in original data set)}
+#'   \item{experiment_time}{intial of researchers who performed the experiment and at what date}
+#'   \item{time}{Time in minutes when blood sugar were measured administering insulin}
 #' }
 #'
-#' @source TODO reference
+#' @source Banting, F. G., Best, C. H., Collip, J. B., Macleod, J. J., & Noble, E. C. (1922). The effect of pancreatic extract (insulin) on normal rabbits. American Journal of Physiology-Legacy Content, 62(1), 162-176.
 "insulin"
 
 #' Darwin data TODO
